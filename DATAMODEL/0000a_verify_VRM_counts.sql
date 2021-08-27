3f92d6807bc701411e390fde1adcd714892fac8a
tlh22 <thancock@mhtc.co.uk>
2021-08-27 07:22:13 +0200

    Update 0000a_verify_VRM_counts.sql

--- DATAMODEL/0000a_verify_VRM_counts.sql
+++ DATAMODEL/0000a_verify_VRM_counts.sql
@@ -6,5 +6,15 @@ SELECT v."SurveyID", s."BeatTitle", COUNT(v."ID")
 FROM demand."VRMs" v, demand."Surveys" s
 WHERE v."SurveyID" = s."SurveyID"
 GROUP BY v."SurveyID", s."BeatTitle"
-ORDER BY v."SurveyID"
+ORDER BY v."SurveyID";
 
+
+-- Use this to get break down by area - and then create a pivot table in Excel
+
+SELECT s."SurveyID", a.name, COUNT(v."ID")
+FROM demand."VRMs" v, demand."Surveys" s, mhtc_operations."Supply" r, mhtc_operations."SurveyAreas" a
+WHERE v."SurveyID" = s."SurveyID"
+AND v."GeometryID" = r."GeometryID"
+AND r."SurveyArea"::int = a.id
+GROUP BY s."SurveyID", a.name
+ORDER BY s."SurveyID", a.name
