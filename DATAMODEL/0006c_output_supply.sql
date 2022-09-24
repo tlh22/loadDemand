e2c156035215b6d33d33492ed2c641c7acbba01b
tlh22 <thancock@mhtc.co.uk>
2022-09-24 17:41:35 +0200

    Changes from Cambridge

--- DATAMODEL/0006c_output_supply.sql
+++ DATAMODEL/0006c_output_supply.sql
@@ -7,22 +7,10 @@
 --
 
 SELECT
-"GeometryID",
-        CASE WHEN "RestrictionTypeID" = 225 THEN
-                 CASE
-                    WHEN "UnacceptableTypeID" IS NOT NULL THEN 220
-                    ELSE 216
-                 END
-                 WHEN "RestrictionTypeID" = 224 THEN
-                 CASE
-                    WHEN "UnacceptableTypeID" IS NOT NULL THEN 221
-                    ELSE 201
-                 END
-             ELSE "RestrictionTypeID"
-        END AS "RestrictionTypeID",
+"GeometryID", "RestrictionTypeID",
 "BayLineTypes"."Description" AS "RestrictionDescription",
 "GeomShapeID", COALESCE("RestrictionGeomShapeTypes"."Description", '') AS "Restriction Shape Description",
-a."RoadName", a."StartStreet" AS "RoadFrom", a."EndStreet" AS "RoadTo", a."SideOfStreet", "RC_Sections_merged"."SectionName", COALESCE(a."SurveyArea", '')  AS "SurveyArea",
+a."RoadName", a."StartStreet" AS "RoadFrom", a."EndStreet" AS "RoadTo", a."SideOfStreet", "RC_Sections_merged"."SectionName", COALESCE("SurveyAreas"."SurveyAreaName", '')  AS "SurveyArea",
 
        CASE WHEN ("RestrictionTypeID" < 200 OR "RestrictionTypeID" IN (227, 228, 229, 231)) THEN COALESCE("TimePeriods1"."Description", '')
             ELSE COALESCE("TimePeriods2"."Description", '')
@@ -41,7 +29,7 @@ a."RoadName", a."StartStreet" AS "RoadFrom", a."EndStreet" AS "RoadTo", a."SideO
             END AS "ParkingAvailableDuringSurveyHours", "CPZ"
 
 FROM
-     ((((((
+     (((((((
      (SELECT CASE WHEN "RestrictionTypeID" = 225 THEN
                  CASE
                     WHEN "UnacceptableTypeID" IS NOT NULL THEN 220
@@ -61,90 +49,16 @@ FROM
      LEFT JOIN "toms_lookups"."TimePeriods" AS "TimePeriods2" ON a."NoWaitingTimeID" is not distinct from "TimePeriods2"."Code")
 	 LEFT JOIN "toms_lookups"."UnacceptableTypes" AS "UnacceptableTypes" ON a."UnacceptableTypeID" is not distinct from "UnacceptableTypes"."Code")
 	 LEFT JOIN "mhtc_operations"."RC_Sections_merged" AS "RC_Sections_merged" ON a."SectionID" is not distinct from "RC_Sections_merged"."gid")
+	 LEFT JOIN "mhtc_operations"."SurveyAreas" AS "SurveyAreas" ON a."SurveyAreaID" is not distinct from "SurveyAreas"."Code"
+	 )
 
 ORDER BY "RestrictionTypeID", "GeometryID"
-
-
---- used in Haringey
---
-
-SELECT
-"GeometryID", "RestrictionTypeID", "BayLineTypes"."Description" AS "RestrictionDescription",
-"GeomShapeID", COALESCE("RestrictionGeomShapeTypes"."Description", '') AS "Restriction Shape Description",
-"RoadName", "StartStreet" AS "RoadFrom", "EndStreet" AS "RoadTo", "SideOfStreet", "SectionID", COALESCE("SurveyArea", ''),
-
-       CASE WHEN "RestrictionTypeID" < 200 THEN COALESCE("TimePeriods1"."Description", '')
-            ELSE COALESCE("TimePeriods2"."Description", '')
-            END  AS "DetailsOfControl",
-       COALESCE("UnacceptableTypes"."Description", '') AS "UnacceptabilityReason",
-       "RestrictionLength" AS "KerblineLength",
-       "NrBays" AS "MarkedBays", "Capacity" AS "TheoreticalBays",
-
-       CASE WHEN "RestrictionTypeID" IN (122, 162, 107, 161, 202, 218, 220, 221, 222, 209, 210, 211, 212, 213, 214, 215) THEN 0
-            --WHEN "RestrictionTypeID" IN (201, 217) THEN
-                --CASE WHEN "Allowable" IS NULL THEN "Capacity"
-                     --ELSE 0
-                     --END
-            ELSE
-                "Capacity"
-            END AS "ParkingAvailableDuringSurveyHours"
-
-FROM
-     (((((mhtc_operations."Supply" AS a
-     LEFT JOIN "toms_lookups"."BayLineTypes" AS "BayLineTypes" ON a."RestrictionTypeID" is not distinct from "BayLineTypes"."Code")
-     LEFT JOIN "toms_lookups"."RestrictionGeomShapeTypes" AS "RestrictionGeomShapeTypes" ON a."GeomShapeID" is not distinct from "RestrictionGeomShapeTypes"."Code")
-     LEFT JOIN "toms_lookups"."TimePeriods" AS "TimePeriods1" ON a."TimePeriodID" is not distinct from "TimePeriods1"."Code")
-     LEFT JOIN "toms_lookups"."TimePeriods" AS "TimePeriods2" ON a."NoWaitingTimeID" is not distinct from "TimePeriods2"."Code")
-	 LEFT JOIN "toms_lookups"."UnacceptableTypes" AS "UnacceptableTypes" ON a."UnacceptableTypeID" is not distinct from "UnacceptableTypes"."Code")
-
-ORDER BY "RestrictionTypeID", "GeometryID"
-
-
---- used in Haringey
-
-SELECT
-"GeometryID", "RestrictionTypeID", "BayLineTypes"."Description" AS "RestrictionDescription",
-"GeomShapeID", COALESCE("RestrictionGeomShapeTypes"."Description", '') AS "Restriction Shape Description",
-a."RoadName", a."StartStreet" AS "RoadFrom", a."EndStreet" AS "RoadTo", a."SideOfStreet", COALESCE("SectionName", '') AS "SectionName", COALESCE(a."SurveyArea", '') AS "SurveyArea",
-
-       CASE WHEN "RestrictionTypeID" < 200 THEN COALESCE("TimePeriods1"."Description", '')
-            ELSE COALESCE("TimePeriods2"."Description", '')
-            END  AS "DetailsOfControl",
-       COALESCE("UnacceptableTypes"."Description", '') AS "UnacceptabilityReason",
-       "RestrictionLength" AS "KerblineLength",
-       "NrBays" AS "MarkedBays", "Capacity" AS "TheoreticalBays",
-
-       CASE WHEN "RestrictionTypeID" IN (107, 122, 161, 162, 202, 209, 210, 211, 212, 213, 214, 215, 218, 220, 221, 222) THEN 0
-            -- exclude bus stops/bus stands
-            -- include acceptable SYLs, SRLs
-            --WHEN "RestrictionTypeID" IN (201, 217) THEN
-                --CASE WHEN "Allowable" IS NULL THEN "Capacity"
-                     --ELSE 0
-                     --END
-            ELSE
-                "Capacity"
-            END AS "ParkingAvailableDuringSurveyHours"
-	, "CPZ", "Notes"
-
-
-FROM
-     ((((((mhtc_operations."Supply" AS a
-     LEFT JOIN "toms_lookups"."BayLineTypes" AS "BayLineTypes" ON a."RestrictionTypeID" is not distinct from "BayLineTypes"."Code")
-     LEFT JOIN "toms_lookups"."RestrictionGeomShapeTypes" AS "RestrictionGeomShapeTypes" ON a."GeomShapeID" is not distinct from "RestrictionGeomShapeTypes"."Code")
-     LEFT JOIN "toms_lookups"."TimePeriods" AS "TimePeriods1" ON a."TimePeriodID" is not distinct from "TimePeriods1"."Code")
-     LEFT JOIN "toms_lookups"."TimePeriods" AS "TimePeriods2" ON a."NoWaitingTimeID" is not distinct from "TimePeriods2"."Code")
-	 LEFT JOIN "toms_lookups"."UnacceptableTypes" AS "UnacceptableTypes" ON a."UnacceptableTypeID" is not distinct from "UnacceptableTypes"."Code")
-	 LEFT JOIN mhtc_operations."RC_Sections_merged" As "Sections" ON a."SectionID" is not distinct from "Sections"."gid")
-
-	WHERE a."CPZ" IN ('FP')
-
-ORDER BY "RestrictionTypeID", "GeometryID"
-
 
 /*
 For car parks
 */
 
+/***
 SELECT
 "GeometryID", "RestrictionTypeID", "RestrictionPolygonTypes"."Description" AS "RestrictionDescription",
 "GeomShapeID", COALESCE("RestrictionGeomShapeTypes"."Description", '') AS "Restriction Shape Description",
@@ -164,12 +78,13 @@ FROM
 WHERE "RestrictionTypeID" = 25 -- car park
 
 ORDER BY "RestrictionTypeID", "GeometryID";
-
+***/
 
 /**
 For sections
 **/
 
+/***
 SELECT
 s."GeometryID", s."RestrictionTypeID", 'Subsection' AS "RestrictionDescription",
 10 AS "GeomShapeID", 'Parallel Line' AS "Restriction Shape Description",
@@ -184,3 +99,4 @@ a."RoadName", a."StartStreet" AS "RoadFrom", a."EndStreet" AS "RoadTo", a."SideO
 FROM "mhtc_operations"."RC_Sections_merged" a, "demand"."SupplyForDemand" s
 WHERE ST_Equals(a.geom, s.geom)
 ORDER BY s."GeometryID";
+***/
--- DATAMODEL/0011_output_VRMs.sql
+++ DATAMODEL/0011_output_VRMs.sql
@@ -1,40 +1,23 @@
 --
 
-SELECT "ID", "SurveyDay", "SurveyID", "SurveyTime", "SurveyDate", "RoadName", "SubID", "GeometryID", "PositionID",
-       "VRM", "VehType", "VehicleTypes"."Description" AS "VehicleType Description"
-       --, "VRMs"."PermitType", "PermitTypes"."PermitType" AS "ParkingType Description", "BRType", "BRTypes"."PermitType" AS "BusRes Description"
-       , "Notes"
-FROM "demand"."VRMs_Watford_Tuesday",
-	(SELECT "Code", "Description"
-      FROM dblink('hostaddr=127.0.0.1 port=5432 dbname=MasterLookups user=postgres password=OS!2postgres options=-csearch_path=',
-		'SELECT "Code", "Description" FROM public."VehicleTypes"') AS "VehicleTypes"("Code" int, "Description" text)) AS "VehicleTypes"
-	--,(SELECT cast("Code" as int), "PermitType" FROM "PC1911_Permits") AS "PermitTypes",
-	--(SELECT cast("Code" as int), "PermitType" FROM "PC1911_BusRes_Permits") AS "BRTypes"
-WHERE "VRM" IS NOT NULL
---AND "VRM" <> '-'
---AND "VRM" <> '_'
---AND "VRM" <> ''
---AND "SurveyDay" = 'Sunday'
-AND "VehicleTypes"."Code" is not distinct from cast("VehType" as int)
---AND "PermitTypes"."Code" is not distinct from "VRMs"."PermitType"
---AND "BRTypes"."Code" is not distinct from "BRType"
-ORDER BY "VRM", "SurveyID"
-
---
-
-SELECT v."ID", v."SurveyID", s."BeatTitle", v."GeometryID", v."RoadName",
-		v."PositionID", v."VRM", v."VRM_Orig", v."VehicleTypeID", v."VehicleType Description",
-        v."RestrictionTypeID", v."RestrictionType Description",
+SELECT v."ID", v."SurveyID", s."SurveyDay", s."BeatStartTime" || '-' || s."BeatEndTime" AS "SurveyTime",
+        v."GeometryID", v."RestrictionTypeID", v."RestrictionType Description",
+        v."RoadName", v."SideOfStreet",
+		v."PositionID", v."VRM", v."VRM_Orig",
+		v."VehicleTypeID", v."VehicleType Description", v."PCU",
         v."PermitTypeID", v."PermitType Description",
-        v."Notes", "Enumerator", "DemandSurveyDateTime"
+        v."Notes", "Enumerator", "DemandSurveyDateTime",
+        r."NrBaysSuspended",
+        CONCAT( COALESCE("SuspensionReference" || '; ', ''), COALESCE("SuspensionReason" || '; ', ''),
+                 COALESCE("SuspensionLength" || '; ', ''), COALESCE("SuspensionNotes" || '; ', '') )
 
 FROM
 (SELECT "ID", "SurveyID", a."GeometryID", "PositionID", "VRM", "VRM_Orig",
-"VehicleTypeID", "VehicleTypes"."Description" AS "VehicleType Description",
+"VehicleTypeID", "VehicleTypes"."Description" AS "VehicleType Description", "VehicleTypes"."PCU" AS "PCU",
        su."RestrictionTypeID",
 		"BayLineTypes"."Description" AS "RestrictionType Description",
         "PermitTypeID", "PermitTypes"."Description" AS "PermitType Description",
-        a."Notes", "RoadName"
+        a."Notes", "RoadName", "SideOfStreet"
 
 FROM
      ((((demand."VRMs" AS a
@@ -52,21 +35,3 @@ AND s."SurveyID" > 0
 --AND su."CPZ" = 'HS'
 --AND s."SurveyID" > 20 and s."SurveyID" < 30
 ORDER BY "GeometryID", "VRM", "SurveyID"
-
-
---
-SELECT "ID", "SurveyID", a."GeometryID", "PositionID", "VRM",
-"VehicleTypeID", "VehicleTypes"."Description" AS "VehicleType Description",
-       su."RestrictionTypeID",
-		"BayLineTypes"."Description" AS "RestrictionType Description",
-        "PermitTypeID", "PermitTypes"."Description" AS "PermitType Description",
-        a."Notes"
-
-FROM
-     ((((demand."VRMs" AS a
-	 LEFT JOIN mhtc_operations."Supply" AS su ON a."GeometryID" = su."GeometryID")
-     LEFT JOIN "toms_lookups"."BayLineTypes" AS "BayLineTypes" ON su."RestrictionTypeID" is not distinct from "BayLineTypes"."Code")
-     LEFT JOIN "demand_lookups"."VehicleTypes" AS "VehicleTypes" ON a."VehicleTypeID" is not distinct from "VehicleTypes"."Code")
-     LEFT JOIN "demand_lookups"."PermitTypes" AS "PermitTypes" ON a."PermitTypeID" is not distinct from "PermitTypes"."Code")
-
-	 WHERE "SurveyID" > 20 and "SurveyID" < 30
