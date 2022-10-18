29e5fb8be9824c8a0a316048a54ad5eb5df45ace
tlh22 <thancock@mhtc.co.uk>
2022-10-18 10:29:39 +0200

    reorder

---
+++ DATAMODEL/Utilities/9994_permissions.sql
@@ -0,0 +1,60 @@
+/***
+ * Set up permisssions for wider use ...
+ ***/
+
+REVOKE ALL ON ALL TABLES IN SCHEMA addresses FROM toms_public, toms_operator, toms_admin;
+GRANT SELECT ON ALL TABLES IN SCHEMA addresses TO toms_public, toms_operator, toms_admin;
+GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA addresses TO toms_public, toms_operator, toms_admin;
+GRANT USAGE ON SCHEMA addresses TO toms_public, toms_operator, toms_admin;
+
+REVOKE ALL ON ALL TABLES IN SCHEMA compliance FROM toms_public, toms_operator, toms_admin;
+GRANT SELECT ON ALL TABLES IN SCHEMA compliance TO toms_public, toms_operator, toms_admin;
+GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA compliance TO toms_public, toms_operator, toms_admin;
+GRANT USAGE ON SCHEMA compliance TO toms_public, toms_operator, toms_admin;
+
+REVOKE ALL ON ALL TABLES IN SCHEMA compliance_lookups FROM toms_public, toms_operator, toms_admin;
+GRANT SELECT ON ALL TABLES IN SCHEMA compliance_lookups TO toms_public, toms_operator, toms_admin;
+GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA compliance_lookups TO toms_public, toms_operator, toms_admin;
+GRANT USAGE ON SCHEMA compliance_lookups TO toms_public, toms_operator, toms_admin;
+
+REVOKE ALL ON ALL TABLES IN SCHEMA demand FROM toms_public, toms_operator, toms_admin;
+GRANT SELECT ON ALL TABLES IN SCHEMA demand TO toms_public, toms_operator, toms_admin;
+GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA demand TO toms_public, toms_operator, toms_admin;
+GRANT USAGE ON SCHEMA demand TO toms_public, toms_operator, toms_admin;
+
+REVOKE ALL ON ALL TABLES IN SCHEMA demand_lookups FROM toms_public, toms_operator, toms_admin;
+GRANT SELECT ON ALL TABLES IN SCHEMA demand_lookups TO toms_public, toms_operator, toms_admin;
+GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA demand_lookups TO toms_public, toms_operator, toms_admin;
+GRANT USAGE ON SCHEMA demand_lookups TO toms_public, toms_operator, toms_admin;
+
+REVOKE ALL ON ALL TABLES IN SCHEMA highways_network FROM toms_public, toms_operator, toms_admin;
+GRANT SELECT ON ALL TABLES IN SCHEMA highways_network TO toms_public, toms_operator, toms_admin;
+GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA highways_network TO toms_public, toms_operator, toms_admin;
+GRANT USAGE ON SCHEMA highways_network TO toms_public, toms_operator, toms_admin;
+
+REVOKE ALL ON ALL TABLES IN SCHEMA local_authority FROM toms_public, toms_operator, toms_admin;
+GRANT SELECT ON ALL TABLES IN SCHEMA local_authority TO toms_public, toms_operator, toms_admin;
+GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA local_authority TO toms_public, toms_operator, toms_admin;
+GRANT USAGE ON SCHEMA local_authority TO toms_public, toms_operator, toms_admin;
+
+REVOKE ALL ON ALL TABLES IN SCHEMA mhtc_operations FROM toms_public, toms_operator, toms_admin;
+GRANT SELECT ON ALL TABLES IN SCHEMA mhtc_operations TO toms_public, toms_operator, toms_admin;
+GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA mhtc_operations TO toms_public, toms_operator, toms_admin;
+GRANT USAGE ON SCHEMA mhtc_operations TO toms_public, toms_operator, toms_admin;
+
+REVOKE ALL ON ALL TABLES IN SCHEMA toms_lookups FROM toms_public, toms_operator, toms_admin;
+GRANT SELECT ON ALL TABLES IN SCHEMA toms_lookups TO toms_public, toms_operator, toms_admin;
+GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA toms_lookups TO toms_public, toms_operator, toms_admin;
+GRANT USAGE ON SCHEMA toms_lookups TO toms_public, toms_operator, toms_admin;
+
+REVOKE ALL ON ALL TABLES IN SCHEMA topography FROM toms_public, toms_operator, toms_admin;
+GRANT SELECT ON ALL TABLES IN SCHEMA topography TO toms_public, toms_operator, toms_admin;
+GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA topography TO toms_public, toms_operator, toms_admin;
+GRANT USAGE ON SCHEMA topography TO toms_public, toms_operator, toms_admin;
+
+--- TOMs main tables
+
+REVOKE ALL ON ALL TABLES IN SCHEMA toms FROM toms_public, toms_operator, toms_admin;
+GRANT SELECT ON ALL TABLES IN SCHEMA toms TO toms_public, toms_operator, toms_admin;
+GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA toms TO toms_public, toms_operator, toms_admin;
+GRANT USAGE ON SCHEMA toms TO toms_public, toms_operator, toms_admin;
