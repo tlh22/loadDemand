4d9a0e9582c1ed4f7aab00ae09d97dcc74e20297
tlh22 <thancock@mhtc.co.uk>
2023-01-27 21:34:55 +0100

    Update 2013_add_new_supply_to_RiS.sql

--- DATAMODEL/002_Counts/RBKC/2013_add_new_supply_to_RiS.sql
+++ DATAMODEL/002_Counts/RBKC/2013_add_new_supply_to_RiS.sql
@@ -9,3 +9,9 @@ FROM demand."Surveys",
 FROM mhtc_operations."Supply"
 WHERE "GeometryID" NOT IN (SELECT DISTINCT "GeometryID"
                            FROM demand."RestrictionsInSurveys")) AS r
+
+INSERT INTO "demand"."Counts" ("SurveyID", "GeometryID")
+SELECT "SurveyID", "GeometryID"
+FROM mhtc_operations."Supply" r, demand."Surveys"
+WHERE "GeometryID" NOT IN (SELECT DISTINCT "GeometryID"
+                           FROM demand."Counts")
