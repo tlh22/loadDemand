7a3ba94d777f35c7b8b3847e15d5f6bbde97aacc
tlh22 <thancock@mhtc.co.uk>
2023-01-30 12:35:46 +0100

    Update 0002_DemandSurveyDates.sql

--- DATAMODEL/002_Counts/RBKC/Reports/0002_DemandSurveyDates.sql
+++ DATAMODEL/002_Counts/RBKC/Reports/0002_DemandSurveyDates.sql
@@ -1,6 +1,7 @@
 -- Dates for demand by street
 
-SELECT RiS."SurveyID", su."BeatTitle", s."RoadName",TO_CHAR( MIN(RiS."DemandSurveyDateTime"), 'dd/mm/yyyy')
+/***
+SELECT RiS."SurveyID", su."BeatTitle", s."RoadName",TO_CHAR( MIN(RiS."DemandSurveyDateTime"), 'dd/mm/yyyy')  AS "DemandSurveyDate"
 FROM demand."RestrictionsInSurveys" RiS, mhtc_operations."Supply" s, demand."Surveys" su
 WHERE RiS."GeometryID" = s."GeometryID"
 AND RiS."SurveyID" = su."SurveyID"
@@ -8,10 +9,22 @@ AND RiS."SurveyID" > 0
 GROUP BY RiS."SurveyID", su."BeatTitle", s."RoadName"
 ORDER BY s."RoadName", RiS."SurveyID"
 
+***/
 
 -- can we do this as a crosstab query?
 
+
+
 CREATE EXTENSION IF NOT EXISTS tablefunc;
 
-SELECT * FROM crosstab('...')
-    AS ct(row_name text, extra text, cat1 text, cat2 text, cat3 text, cat4 text);
+SELECT * FROM crosstab('
+SELECT s."RoadName"::text, su."BeatTitle"::text, TO_CHAR( MIN(RiS."DemandSurveyDateTime"), ''dd/mm/yyyy'')::text AS "DemandSurveyDate"
+FROM demand."RestrictionsInSurveys" RiS, mhtc_operations."Supply" s, demand."Surveys" su
+WHERE RiS."GeometryID" = s."GeometryID"
+AND RiS."SurveyID" = su."SurveyID"
+AND RiS."SurveyID" > 0
+GROUP BY su."BeatTitle", s."RoadName"
+ORDER BY s."RoadName", su."BeatTitle"
+')
+    AS ct("RoadName" text, "Weekday Overnight" text, "Weekday Afternoon" text, "Weekday Evening" text, 
+	 "Saturday Afternoon" text, "Sunday Afternoon" text);
