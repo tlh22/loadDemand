572bb72d72eb009fee103899b2ac4d0810404ab3
tlh22 <thancock@mhtc.co.uk>
2025-02-04 11:02:55 +0100

    move files around

--- DATAMODEL/0004_photos_with_details_from_RiS_demand.sql
+++ DATAMODEL/0004_photos_with_details_from_RiS_demand.sql
@@ -68,3 +68,66 @@ AND a."GeometryID" = RiS."GeometryID"
 AND COALESCE(RiS."NrBaysSuspended", 0) = 0
 --AND s."SurveyID" in ( SELECT "SurveyID" FROM demand."Surveys" WHERE "SiteArea" LIKE 'FP%')
 
+
+
+
+
+SELECT CONCAT('copy ', g."Photo", ' "', '../Extra_Demand_Photos/', 'Photo_', to_char(g."sid", 'fm000'), '_', "RoadName",  '.', ext, '"')
+FROM
+(
+SELECT row_number() OVER (PARTITION BY true::boolean) AS sid,
+	b."RoadName", b."Photo", substring(b."Photo", '(?<=\.)[^\]]+') AS ext
+FROM
+(
+SELECT su."RoadName", su."Photos_01" AS "Photo"
+FROM demand."DemandPhotos" su
+WHERE su."Photos_01" IS NOT NULL
+ORDER BY "Photo"
+
+FROM demand."RestrictionsInSurveys" RiS, demand."Surveys" s,
+(SELECT su."GeometryID", r."SectionName"
+FROM mhtc_operations."RC_Sections_merged" r, mhtc_operations."Supply" su
+WHERE r."gid" = su."SectionID"
+--AND su."CPZ" = 'FPC'
+) a
+WHERE "Photos_01" IS NOT NULL
+AND RiS."SurveyID" = s."SurveyID"
+AND a."GeometryID" = RiS."GeometryID"
+AND COALESCE(RiS."NrBaysSuspended", 0) = 0
+--AND s."SurveyID" in ( SELECT "SurveyID" FROM demand."Surveys" WHERE "SiteArea" LIKE 'FP%')
+
+UNION
+
+SELECT CONCAT('copy ', RiS."Photos_02", ' "../Photos_Demand/', a."SectionName", '_', s."BeatTitle", '_', RiS."Photos_02", '"')
+FROM demand."RestrictionsInSurveys" RiS, demand."Surveys" s,
+(SELECT su."GeometryID", r."SectionName"
+FROM mhtc_operations."RC_Sections_merged" r, mhtc_operations."Supply" su
+WHERE r."gid" = su."SectionID"
+--AND su."CPZ" = 'FPC'
+) a
+WHERE "Photos_02" IS NOT NULL
+AND RiS."SurveyID" = s."SurveyID"
+AND a."GeometryID" = RiS."GeometryID"
+AND COALESCE(RiS."NrBaysSuspended", 0) = 0
+--AND s."SurveyID" in ( SELECT "SurveyID" FROM demand."Surveys" WHERE "SiteArea" LIKE 'FP%')
+
+UNION
+
+SELECT CONCAT('copy ', RiS."Photos_03", ' "../Photos_Demand/', a."SectionName", '_', s."BeatTitle", '_', RiS."Photos_03", '"')
+FROM demand."RestrictionsInSurveys" RiS, demand."Surveys" s,
+(SELECT su."GeometryID", r."SectionName"
+FROM mhtc_operations."RC_Sections_merged" r, mhtc_operations."Supply" su
+WHERE r."gid" = su."SectionID"
+--AND su."CPZ" = 'FPC'
+) a
+WHERE "Photos_03" IS NOT NULL
+AND RiS."SurveyID" = s."SurveyID"
+AND a."GeometryID" = RiS."GeometryID"
+AND COALESCE(RiS."NrBaysSuspended", 0) = 0
+--AND s."SurveyID" in ( SELECT "SurveyID" FROM demand."Surveys" WHERE "SiteArea" LIKE 'FP%')
+
+
+
+
+) b
+) g;
