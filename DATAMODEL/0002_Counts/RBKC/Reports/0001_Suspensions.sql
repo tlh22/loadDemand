38048e02cbfab3fce61b5a299e61aa10711d23bc
tlh22 <thancock@mhtc.co.uk>
2025-03-08 12:22:00 +1000

    Update 0001_Suspensions.sql

--- DATAMODEL/0002_Counts/RBKC/Reports/0001_Suspensions.sql
+++ DATAMODEL/0002_Counts/RBKC/Reports/0001_Suspensions.sql
@@ -39,3 +39,38 @@ AND COALESCE("NrBaysSuspended", 0) > 0
 AND s."RestrictionTypeID" < 200
 AND ris."SurveyID" = su."SurveyID"
 ORDER BY s."RoadName", ris."NrBaysSuspended"
+
+--
+
+SELECT ris."GeometryID", su."SurveyID", su."SurveyDay" AS "BeatTItle", s."RoadName", s."RestrictionType Description", 
+    ris."SuspensionReference", ris."SuspensionReason", '' AS "Revised Suspension Reason", ris."NrBaysSuspended", ris."SuspensionNotes"
+
+FROM demand."RestrictionsInSurveys" ris, demand."Surveys" su, demand."Counts" c,
+(
+SELECT a."GeometryID", a."RestrictionTypeID", a."RestrictionLength", a."Capacity", "UnacceptableTypes"."Description" AS "UnacceptableType Description", "BayLineTypes"."Description" AS "RestrictionType Description",
+a."RoadName", a."CPZ", "SurveyAreas"."SurveyAreaName", "Wards"."Name" AS "WardName", "ParkingTariffZones"."tariff_cat" AS "ParkingTariffZoneName",
+'' AS "HospitalZonesBlueBadgeHoldersName", l.item_refs,
+ a.geom AS "SupplyGeom"
+ FROM
+((((((  mhtc_operations."Supply" AS a
+ LEFT JOIN "toms_lookups"."BayLineTypes" AS "BayLineTypes" ON a."RestrictionTypeID" is not distinct from "BayLineTypes"."Code")
+ LEFT JOIN "local_authority"."Wards_2022" AS "Wards" ON a."WardID" is not distinct from "Wards"."id")
+ LEFT JOIN "local_authority"."PayByPhoneTariffZones" AS "ParkingTariffZones" ON a."ParkingTariffZoneID" is not distinct from "ParkingTariffZones"."id")
+ -- LEFT JOIN "local_authority"."HospitalZonesBlueBadgeHolders_2022" AS "HospitalZonesBlueBadgeHolders" ON a."HospitalZonesBlueBadgeHoldersID" is not distinct from "HospitalZonesBlueBadgeHolders"."id")
+ LEFT JOIN "mhtc_operations"."SurveyAreas" AS "SurveyAreas" ON a."SurveyAreaID" is not distinct from "SurveyAreas"."Code")
+ LEFT JOIN "toms_lookups"."UnacceptableTypes" AS "UnacceptableTypes" ON a."UnacceptableTypeID" is not distinct from "UnacceptableTypes"."Code")
+ LEFT JOIN (SELECT "GeometryID" AS "GeometryID_Links", ARRAY_AGG ("item_ref") AS item_refs
+											 FROM mhtc_operations."RBKC_item_ref_links_2024"
+											 GROUP BY "GeometryID" ) AS l ON a."GeometryID" = l."GeometryID_Links")
+ ) AS s
+ WHERE ris."SurveyID" = su."SurveyID"
+ AND ris."GeometryID" = s."GeometryID"
+ AND ris."SurveyID" = c."SurveyID"
+ AND ris."GeometryID" = c."GeometryID"
+ AND su."SurveyID" > 0
+ AND COALESCE(ris."NrBaysSuspended", 0) > 0
+ --AND s."RestrictionTypeID" NOT IN (116, 117, 118, 119, 144, 147, 149, 150, 168, 169)  -- MCL, PCL, Scooters, etc
+ --AND RiS."Done" IS True
+  -- AND s."RoadName" LIKE 'Lower Addison Gardens%'
+
+ORDER BY s."RoadName", ris."NrBaysSuspended";
