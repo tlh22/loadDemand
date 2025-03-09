a7f3b4746f5302376640b63a884e6c63090cc189
tlh22 <thancock@mhtc.co.uk>
2025-03-09 19:34:42 +1000

    dual restricfions

--- DATAMODEL/0002_Counts/Utilities/0003_DualRestrictions_Report.sql
+++ DATAMODEL/0002_Counts/Utilities/0003_DualRestrictions_Report.sql
@@ -7,32 +7,29 @@ s1."RoadName",
             END  AS "Secondary DetailsOfControl",
         CASE WHEN (s2."RestrictionTypeID" < 200 OR s2."RestrictionTypeID" IN (227, 228, 229, 231)) THEN COALESCE(s2."TimePeriod", '')
             ELSE COALESCE(s2."NoWaitingTime", '')
-            END  AS "Primary DetailsOfControl",
-        s1.item_refs
+            END  AS "Primary DetailsOfControl"
+			--,
+        --s1.item_refs
  FROM
  mhtc_operations."DualRestrictions" d,
 (
  SELECT "GeometryID", "RestrictionTypeID", "BayLineTypes"."Description" AS "RestrictionType Description", "RoadName",
         "TimePeriodID", "TimePeriods1"."Description" AS "TimePeriod",
-        "NoWaitingTimeID", "TimePeriods2"."Description" AS "NoWaitingTime", l.item_refs
+        "NoWaitingTimeID", "TimePeriods2"."Description" AS "NoWaitingTime" --, l.item_refs
  FROM mhtc_operations."Supply" AS a
  LEFT JOIN "toms_lookups"."BayLineTypes" AS "BayLineTypes" ON a."RestrictionTypeID" is not distinct from "BayLineTypes"."Code"
  LEFT JOIN "toms_lookups"."TimePeriods" AS "TimePeriods1" ON a."TimePeriodID" is not distinct from "TimePeriods1"."Code"
  LEFT JOIN "toms_lookups"."TimePeriods" AS "TimePeriods2" ON a."NoWaitingTimeID" is not distinct from "TimePeriods2"."Code"
- LEFT JOIN (SELECT "GeometryID" AS "GeometryID_Links", ARRAY_AGG ("item_ref") AS item_refs
-											 FROM mhtc_operations."RBKC_item_ref_links_2024"
-											 GROUP BY "GeometryID" ) AS l ON a."GeometryID" = l."GeometryID_Links") AS s1,
+ ) AS s1,
 (
  SELECT "GeometryID", "RestrictionTypeID", "BayLineTypes"."Description" AS "RestrictionType Description", "RoadName",
         "TimePeriodID", "TimePeriods1"."Description" AS "TimePeriod",
-        "NoWaitingTimeID", "TimePeriods2"."Description" AS "NoWaitingTime", l.item_refs
+        "NoWaitingTimeID", "TimePeriods2"."Description" AS "NoWaitingTime" --, l.item_refs
  FROM mhtc_operations."Supply" AS a
  LEFT JOIN "toms_lookups"."BayLineTypes" AS "BayLineTypes" ON a."RestrictionTypeID" is not distinct from "BayLineTypes"."Code"
  LEFT JOIN "toms_lookups"."TimePeriods" AS "TimePeriods1" ON a."TimePeriodID" is not distinct from "TimePeriods1"."Code"
  LEFT JOIN "toms_lookups"."TimePeriods" AS "TimePeriods2" ON a."NoWaitingTimeID" is not distinct from "TimePeriods2"."Code"
- LEFT JOIN (SELECT "GeometryID" AS "GeometryID_Links", ARRAY_AGG ("item_ref") AS item_refs
-											 FROM mhtc_operations."RBKC_item_ref_links_2024"
-											 GROUP BY "GeometryID" ) AS l ON a."GeometryID" = l."GeometryID_Links") AS s2
+ ) AS s2
  WHERE d."GeometryID" = s1."GeometryID"
  AND d."LinkedTo" = s2."GeometryID"
  ORDER BY s1."RoadName", s1."RestrictionType Description"
